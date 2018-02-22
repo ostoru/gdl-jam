e866ddb3c294278355ec8d7cd4ea64947b9ec148
OSTORu <OSTORu@cock.li>
2018-02-22 16:50:09 -0600

    movement (terribly) done

--- game/scenes/characters/human.gd
+++ game/scenes/characters/human.gd
@@ -1,7 +1,8 @@
 extends RigidBody
-
+export var active = false
 func _ready():
-	set_process_input(true)
+	if active:
+		set_process_input(true)
 	pass
 
 
@@ -14,23 +15,22 @@ func _physics_process(delta):
 	var yaw_offset = Vector3()
 	if Input.is_action_pressed("move_left"):
 		yaw_offset.x = 1
-		offset_distance += .01
+		offset_distance += .001
 	elif Input.is_action_pressed("move_right"):
 		yaw_offset.x = -1
-		offset_distance += .01
+		offset_distance += .001
 	else:
-		offset_distance -= .01
+		offset_distance -= .0004
 	if Input.is_action_pressed("move_forward"):
 		yaw_offset.z = 1
-		offset_distance += .01
+		offset_distance += .001
 	elif Input.is_action_pressed("move_backwards"):
 		yaw_offset.z = -1
-		offset_distance += .01
+		offset_distance += .001
 	else:
-		offset_distance -= .01
+		offset_distance -= .0004
 	offset_distance = max(.5,min(.1,offset_distance))
 	yaw_offset = yaw_offset.normalized()
-	$yaw.set_translation(($yaw.get_translation() - yaw_offset) * offset_distance)
-
-func _integrate_forces(state):
-	$yaw.get_global_transform().origin - $".".get_global_transform().origin
+	$yaw/pitch.set_translation(($yaw.get_translation() - yaw_offset) * offset_distance)
+	if active:
+		set_translation($".".get_global_transform().origin + (($".".get_global_transform().origin - $yaw/pitch.get_global_transform().origin).normalized() * offset_distance))
